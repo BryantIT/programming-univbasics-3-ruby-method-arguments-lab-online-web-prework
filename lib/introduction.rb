3a02aa1ab703599b82fdb613725277a2ad5409c7
Bryant Richards <dragonseyeimages@gmail.com>
2019-08-08 00:47:26 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,9 +1,9 @@
 def introduction (name)
   puts "Hi, my name is #{name}"
 def introduction_with_language (name,language)
-puts "Hi, my name is #{name} and I am learning to program in #{language}"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
 def introduction_with_language_optional (name, language=Ruby)
-  puts
+  puts 
 
 
 
