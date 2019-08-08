6e9fb2d09e3c2bb489631710304cc3feaaeed914
Bryant Richards <dragonseyeimages@gmail.com>
2019-08-08 00:51:10 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,9 @@
-def introduction (Josh)
+def introduction (name)
   puts "Hi, my name is #{name}"
+
 def introduction_with_language (name,language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
+
 def introduction_with_language_optional (name, language=Ruby)
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
